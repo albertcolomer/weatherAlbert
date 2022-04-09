a3407b7e1c8eee4011d5665d2aeb4da383cc09f8
albertcolomer <colomermr@hotmail.com>
2022-04-09 21:14:37 +0200

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  openWeather: {
+    key: '2580b7a5c4da9450f8783253e110caf7',//'1a8c61f0f54f9eb40100d8184a38a12e',
+    url: 'https://api.openweathermap.org/data/2.5'
+  }
+};
